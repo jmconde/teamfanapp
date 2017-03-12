f27dacb886179ede5defa1129efb7e4c65202c85
Jose Conde <Jose Conde>
2017-03-12 13:00:38 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var restify = require('restify');
+var router = require("./router.js");
+
+function respond(req, res, next) {
+  res.send('hello ' + req.params.name);
+  next();
+}
+
+var server = restify.createServer();
+
+router(server);
+
+server.listen(8080, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
