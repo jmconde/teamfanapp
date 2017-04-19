1ba044713f47824ea94de8a2ac3cd5e8e658855e
Jose Conde <jose.conde@morningstar.com>
2017-04-18 19:14:06 -0500

    add

--- client/components/EquipoEdit.jsx
+++ client/components/EquipoEdit.jsx
@@ -3,15 +3,6 @@ import FormBase from "./FormBasejsx";
 export default class PaisForm extends FormBase {
     constructor(props) {
         super(props);
-        let update = this.props.update || !!this.props.location.search;
-        this.state = {
-            id: this.props.location.search.substring(1) || "",
-            disabled: true,
-            update: update,
-        };
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleSelectChange = this.handleSelectChange.bind(this);
-        this.submitForm = this.submitForm.bind(this);
     }
 
     componentDidMount() {
@@ -58,15 +49,6 @@ export default class PaisForm extends FormBase {
         } else if (name === "ciudad") {
             this.setState({ ciudad: value });
         }
-    }
-
-    handleInputChange(event) {
-        const target = event.target;
-        const value = target.type === "checkbox" ? target.checked : target.value;
-        const name = target.name;
-        this.setState({
-            [name]: value
-        });
     }
 
     submitForm(event) {
